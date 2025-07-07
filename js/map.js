553b4711bb2cb739dd272b9df2170b16fe9cae8b
rodrigocorbalan@gmail.com <rodrigocorbalan@gmail.com>
2025-07-07 17:00:11 -0300

    Zoom mapa

--- js/map.js
+++ js/map.js
@@ -8,7 +8,7 @@ function initMap(mapId) {
         fullscreenControlOptions: {
             position: 'topright'
         }
-    }).setView(homeCoords, 13);
+    }).setView(homeCoords, 9);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
