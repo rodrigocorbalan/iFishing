de557651c941bf6baa708897a2ac1b7d3d877465
rodrigocorbalan@gmail.com <rodrigocorbalan@gmail.com>
2025-07-07 11:44:37 -0300

    moldura map

--- js/map.js
+++ js/map.js
@@ -1,9 +1,6 @@
 let map = null;
 const markers = []; // Array para guardar os marcadores e poder limpá-los
 
-/**
- * Inicializa o mapa Leaflet na div especificada.
- */
 function initMap(mapId) {
     map = L.map(mapId).setView([-23.5505, -46.6333], 8);
 
@@ -11,25 +8,18 @@ function initMap(mapId) {
         attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    // Solução definitiva para o problema de sobreposição do modal
     document.getElementById(mapId).style.zIndex = 0;
 }
 
-/**
- * Limpa todos os marcadores existentes do mapa.
- */
 function clearMarkers() {
     markers.forEach(marker => {
         map.removeLayer(marker);
     });
-    markers.length = 0; // Esvazia o array de marcadores
+    markers.length = 0;
 }
 
-/**
- * Adiciona marcadores customizados (pino + etiqueta com nome) ao mapa.
- */
 function addMarkersToMap(pesqueiros, onMarkerClick) {
-    clearMarkers(); // Limpa os marcadores antigos
+    clearMarkers();
 
     const colors = ['#e6194B', '#3cb44b', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9A6324'];
 
@@ -40,9 +30,8 @@ function addMarkersToMap(pesqueiros, onMarkerClick) {
 
             const pinColor = colors[index % colors.length];
 
-            // Cria um ícone customizado usando L.divIcon com o pino e a etiqueta
             const customIcon = L.divIcon({
-                className: '', // Não precisa de classe wrapper
+                className: '',
                 html: `
                     <div class="custom-pin-label-marker">
                         <svg width="40" height="40" viewBox="0 0 384 512" xmlns="http://www.w3.org/2000/svg">
@@ -53,9 +42,9 @@ function addMarkersToMap(pesqueiros, onMarkerClick) {
                         </div>
                     </div>
                 `,
-                iconSize: null, // Tamanho será definido pelo conteúdo
-                iconAnchor: [20, 40], // Ancoragem na ponta inferior do pino
-                popupAnchor: [0, -35] // Posição de onde o popup deve abrir
+                iconSize: [200, 42], // Tamanho fixo para dar estabilidade
+                iconAnchor: [20, 42], // Ancoragem na ponta inferior do pino
+                popupAnchor: [0, -40]
             });
 
             const marker = L.marker([lat, lng], { icon: customIcon }).addTo(map);
@@ -65,7 +54,7 @@ function addMarkersToMap(pesqueiros, onMarkerClick) {
                     <b>${p.NomePesqueiro}</b><br>
                     <small>${p.CidadeUF}</small>
                     <hr style="margin: 5px 0;">
-                    <a href="https://www.google.com/maps/dir/?api=1&destination=$${lat},${lng}" target="_blank" style="color: #4285F4;">Rota com Google Maps</a><br>
+                    <a href="http://googleusercontent.com/maps.google.com/3{lat},${lng}" target="_blank" style="color: #4285F4;">Rota com Google Maps</a><br>
                     <a href="https://waze.com/ul?ll=${lat},${lng}&navigate=yes" target="_blank" style="color: #33ccff;">Rota com Waze</a><br>
                     <a href="#" class="details-link" data-id="${p.ID}" style="color: #0056b3; font-weight: bold;">Ver mais detalhes</a>
                 </div>
